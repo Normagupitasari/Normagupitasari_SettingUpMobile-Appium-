9b094396d03dd237f3ea9c5be555d1e5a736819c
Norma Gupitasari <normagupitasari02@gmail.com>
2025-07-05 01:17:30 +0700

    Setting Up Mobile Testing with Appium

---
+++ test/appdemo.test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('API Demo Test', () => {
+  it('should open the app', async () => {
+    console.log('Opening the app...');
+    expect(true).to.equal(true); 
+    });
+});
